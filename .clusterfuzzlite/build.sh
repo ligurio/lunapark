f15f0d4bbf00e49f50f4eeaf8c27a8026e6218bb
Sergey Bronnikov <estetus@gmail.com>
2026-02-20 18:58:28 +0300

    cfl: support luzer-based tests
    See also google/oss-fuzz#14656
    See also google/oss-fuzz#14859
    See also tarantool/tarantool#11250

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -29,6 +29,7 @@ esac
 cmake_args=(
     -DUSE_LUAJIT=ON
     -DOSS_FUZZ=ON
+    -DENABLE_LAPI_TESTS=ON
     $SANITIZERS_ARGS
 
     # C compiler
@@ -87,3 +88,55 @@ do
     zip -urj $OUT/"$name"_seed_corpus.zip "$options_path"
   fi
 done
+
+# Code coverage is not supported.
+if [[ "$SANITIZER" == "coverage" ]]; then
+  exit
+fi
+
+LUA_RUNTIME_NAME=luajit
+LUAJIT_PATH=build/luajit-v2.1/source/src/$LUA_RUNTIME_NAME
+LUA_MODULES_DIR=lua_modules
+
+apt install -y luarocks liblua5.1-0 liblua5.1-0-dev liblua5.1-0-dbg lua5.1
+
+# Required by luzer installed using luarocks.
+export OSS_FUZZ=1
+luarocks install --lua-version 5.1 --server=https://luarocks.org/dev --tree=$LUA_MODULES_DIR luzer
+unset OSS_FUZZ
+
+cp tests/lapi/lib.lua $OUT
+
+LUZER_TESTS_DIR="tests/lapi/"
+# Generating test wrappers for luzer-based tests.
+for test_path in $(find $LUZER_TESTS_DIR -name "*_test.lua" -type f);
+do
+  test_file=$(basename $test_path);
+  test_name_we="${test_file%.*}";
+  # The following tests made for the functions that unsupported by
+  # LuaJIT.
+  if [[ $test_name_we == "math_tointeger_test" ||
+        $test_name_we == "math_ult_test" ||
+        $test_name_we == "string_pack_test" ||
+        $test_name_we == "string_packsize_test" ||
+        $test_name_we == "string_unpack_test" ||
+        $test_name_we == "table_pack_test" ||
+        $test_name_we == "utf8_char_test" ||
+        $test_name_we == "utf8_codepoint_test" ||
+        $test_name_we == "utf8_codes_test" ||
+        $test_name_we == "utf8_len_test" ||
+        $test_name_we == "utf8_offset_test" ]]; then
+    continue
+  fi
+  module_name=$(echo $test_name_we | sed 's/_test//' )
+  "$SRC/compile_lua_fuzzer" "$LUA_RUNTIME_NAME" $test_file
+  cp "$test_path" "$OUT/"
+  corpus_dir="corpus/corpus/$test_name_we"
+  if [ -e "$corpus_dir" ]; then
+    zip -j $OUT/"$test_name_we"_seed_corpus.zip $corpus_dir/*
+    echo "Build corpus '$OUT/"$test_name_we"_seed_corpus.zip' for the test '$test_name_we'"
+  fi
+done
+
+cp $LUAJIT_PATH "$OUT/$LUA_RUNTIME_NAME"
+cp -R $LUA_MODULES_DIR "$OUT/"
