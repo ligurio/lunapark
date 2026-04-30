09e8a0777f59c48eabd385b3e6f301552c21471c
Sergey Bronnikov <estetus@gmail.com>
2026-04-30 21:35:28 +0300

    cfl: enable quiet mode for zip
    There are a lot of files in the seed corpuses, and when zip prints
    which files it added to the archives, this greatly increases the
    size of the logs. The patch suppresses this output.

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -132,7 +132,7 @@ do
   corpus_dir="corpus/corpus/$test_name_we"
   if [ -e "$corpus_dir" ]; then
     echo "Build corpus '$OUT/"$test_name_we"_seed_corpus.zip' for the test '$test_name_we'"
-    find "$corpus_dir" -mindepth 1 -maxdepth 1 | zip -@ -j $OUT/"$test_name_we"_seed_corpus.zip
+    find "$corpus_dir" -mindepth 1 -maxdepth 1 | zip --quiet -@ -j $OUT/"$test_name_we"_seed_corpus.zip
   fi
 done
 
